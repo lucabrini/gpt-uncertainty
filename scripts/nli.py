fc2cbd60ac5404d4bd1fcf21408e9aa54bc72aef
Luca Brini <brini.luca@icloud.com>
2023-12-13 20:45:29 +0100

    changed model for NLI, check mps utility

--- scripts/nli.py
+++ scripts/nli.py
@@ -1,8 +1,8 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v3-base")
-model = AutoModelForSequenceClassification.from_pretrained("microsoft/deberta-v3-base").to("mps")
+tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-large-mnli")
+model = AutoModelForSequenceClassification.from_pretrained("microsoft/deberta-large-mnli").to("mps")
 
 def compute_mean_similarity(y : str, yi : str) -> float:
   
@@ -15,17 +15,19 @@ def compute_mean_similarity(y : str, yi : str) -> float:
 
 def compute_similarity(x: str, y: str):
 
-  sequence = (x, y)
+  sequence = x + " [SEP] " + y
   
   encoded_input = tokenizer.encode(sequence, padding=True)
   
-  prediction = model(torch.tensor(torch.tensor([encoded_input]), device='mps'))['logits']
+  prediction = model(torch.tensor(torch.tensor([encoded_input]), device="mps"))['logits']
 
-  probabilities = torch.argmax(prediction, dim=1)
+  predicted_label = torch.argmax(prediction, dim=1)
   
   #p_contradiction = probabilities[0, tokenizer.convert_tokens_to_ids("contradiction")]
-  print("\n\n\n\n")
-  print(probabilities)
+  
+  print(predicted_label)
+  print("semantically different" if 0 in predicted_label else "semantically equals")
   
   
-compute_mean_similarity("The capital of France is Paris", "The France's Capital is Paris")
+  
+compute_mean_similarity("the capital of France is Paris", "The France's Capital is Paris")
