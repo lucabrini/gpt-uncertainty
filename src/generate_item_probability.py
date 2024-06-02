91aa2982cd4fe3df808734ea09587b174a19674a
Emamasi-uni <e.masiero@campus.unimib.it>
2024-06-02 19:54:56 +0200

    Add dialogues valid candidates

--- src/generate_item_probability.py
+++ src/generate_item_probability.py
@@ -1,11 +1,13 @@
 import os
+import re
 import csv
 import json
 from dotenv import load_dotenv
 from string import Template
 from tqdm import tqdm
 import openai
-
+from collections import defaultdict
+import string
 
 def main():
   load_dotenv()
@@ -45,16 +47,40 @@ def main():
       f"-{question} {answer}"
     )
     
-    remaining_candidates = list_remaining_candidates(history, candidates)
+    # remaining_candidates = list_remaining_candidates(history, candidates)
     # print(remaining_candidates)
-    candidates = remaining_candidates
+    # candidates = remaining_candidates
+
+    samples = 5
+    score_list={}
+    remaining_candidates = defaultdict(list)
+    for k in range(samples):
+      remaining_candidates[k] = list_remaining_candidates(history, candidates)
+      print(remaining_candidates[k])
+
+      if remaining_candidates[k]:
+        p = 1 / len(remaining_candidates[k])
+        
+        for item in remaining_candidates[k]:
+          if item in score_list.keys():
+            score_list[item] = score_list[item]+p
+          else:
+            score_list[item] = p
+
+    normalized_scores = {}
+    scores_sum = 0
+    for item in score_list.keys():
+      scores_sum = scores_sum + score_list[item]
+
+    for item in score_list.keys():
+      normalized_scores[item] = round(score_list[item] / scores_sum, 4)
     
     # Computing probability
-    if candidates != []:
-      p = 1 / len(remaining_candidates)
-    else:
-      p = 0
-  
+    # if candidates != []:
+      # p = 1 / len(remaining_candidates)
+    # else:
+      #p = 0
+
     dump_row(
       data_path % "generation",
       dialogue_id,
@@ -62,8 +88,9 @@ def main():
       row["intra_dialogue_id"],
       question,
       answer,
-      remaining_candidates,
-      p
+      list(remaining_candidates.values()),
+      score_list,
+      normalized_scores
     )
 
 
@@ -96,8 +123,8 @@ def open_step_by_step_csv(data_path):
       "question", # question made by the guesser
       "answer",
       "candidates",
-      "probability",
-      "explanation"
+      "score"
+      "score_probability",
     ])
     
 def open_game_sets(data_path):
@@ -106,7 +133,7 @@ def open_game_sets(data_path):
   
 def open_dialogues(data_path):
   rows = []
-  with open(f"{data_path}/dialogues.csv", newline='') as f:
+  with open(f"{data_path}/gpt-dialogues-gpt4.csv", newline='') as f:
     reader = csv.DictReader(f, delimiter=",")
     for row in reader:
       rows.append(row)
@@ -124,8 +151,16 @@ def list_remaining_candidates(history, candidates):
     model='gpt-4o',
     messages=[{'role': "system", 'content': prompt}],
   ).choices[0].message.content
-
-  try:
+  
+  pattern = r"CANDIDATES: ([^;\n]+)"
+  matches = re.findall(pattern, response)
+  candidates = set()
+  for match in matches:
+    candidates_list = match.split(", ")
+    cleaned_candidates = {remove_punctuation(candidate).strip() for candidate in candidates_list}
+    candidates.update(cleaned_candidates)
+  '''
+    try:
     candidates_part = response.split("CANDIDATES: ")[1]
     if candidates_part.strip():
         candidates = candidates_part.split(", ")
@@ -133,13 +168,18 @@ def list_remaining_candidates(history, candidates):
         candidates = []
   except IndexError:
     candidates = []
-  print(candidates)
-  print(response)
+  '''
+  # print(candidates)
+  #print(response)
+  print()
   explanation = response.split("EXPLANATION: ")[1]
   
   return candidates
 
-def dump_row(data_path, dialogue_id, intra_dialogue_id, target, question, answer, candidates, probability):
+def remove_punctuation(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
+
+def dump_row(data_path, dialogue_id, intra_dialogue_id, target, question, answer, candidates, score, score_prob):
   with open(f"{data_path}/dialogues_step_by_step.csv", 'a', newline='') as f:
     write = csv.writer(f)
     write.writerow([
@@ -149,7 +189,8 @@ def dump_row(data_path, dialogue_id, intra_dialogue_id, target, question, answer
       question, 
       answer,
       candidates,
-      probability
+      score,
+      score_prob
     ])
 
 if __name__ == "__main__":
